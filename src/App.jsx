9383bac4c55e6a227084d8848638cc51894f7ebe
dani <danifromecuador@gmail.com>
2024-02-21 17:20:42 -0500

    Add Hero, Footer, and MainPage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './styles/App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
+import { MainPage } from './pages/MainPage.jsx'
+import { Hero } from './components/Hero.jsx'
+import { Footer } from './components/Footer.jsx'
 
 export const App = () => {
   return (
     <Router>
-      <div className="app">
-        < Navbar />
+      <div className="App">
+        <div className="left">     
+          < Hero />     
+          < Navbar />
+          < Footer />
+        </div>
+        <div className="right">
+          < MainPage />
+        </div>
       </div>
     </Router>
   )
