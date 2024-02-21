363c1601365b439f790f437e02eaed5c725fd87c
dani <danifromecuador@gmail.com>
2024-02-21 17:22:40 -0500

    Create Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="theme">
+        picture
+      </div>
+      <div className="hero-name">
+        Hey there, I'm Dani
+      </div>
+      <div className="hero-img">
+        picture
+      </div>
+      <div className="hero-about">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis reprehenderit aliquam aperiam ut commodi, dolores suscipit eligendi quaerat placeat quibusdam, praesentium sequi, ipsam officiis error laudantium quisquam doloremque blanditiis quos.
+      </div>
+    </div>
+  )
+}
