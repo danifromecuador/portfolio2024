95c867ceaa6972025d7448e54b6006afdedcbc66
dani <danifromecuador@gmail.com>
2024-02-22 23:16:53 -0500

    Create main layout for MainPage page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,20 @@
+import '../styles/pages/MainPage.css'
+
 export const MainPage = () => {
   return (
     <div className="main-page">
-      this is the main page
+      <div className="header">
+        header
+      </div>
+      <div className="projects">
+        projects
+      </div>
+      <div className="skills">
+        skills
+      </div>
+      <div className="contact">
+        contact
+      </div>
     </div>
   )
 }
