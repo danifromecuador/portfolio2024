129d65b157eb7b130baff924e6bb5c648f26f39b
dani <danifromecuador@gmail.com>
2024-04-17 23:55:38 -0500

    Add images to change theme

--- src/left/header/Header.jsx
+++ src/left/header/Header.jsx
@@ -1,10 +1,25 @@
 // src/left/header/Header.jsx
+import { useState } from 'react'
 import './Header.css'
 
 export const Header = () => {
+  const [hide1, setHide1] = useState("")
+  const [hide2, setHide2] = useState("hide")
+  const handleHide = () => {
+    if (hide1 === "") {
+      setHide1("hide")
+      setHide2("")
+    } else {
+      setHide1("")
+      setHide2("hide")
+    }
+  }
   return (
     <div className='Header'>
-      Header Component
+      <div className={`day_icon ${hide1}`} onClick={() => handleHide()}>
+      </div>
+      <div className={`night_icon ${hide2}`} onClick={() => handleHide()}>
+      </div>
     </div>
   )
 }
