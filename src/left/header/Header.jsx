454c0ebc3f7ad92d3edc31d1e56e95a2baccbb0f
dani <danifromecuador@gmail.com>
2024-04-18 12:57:05 -0500

    Change colors of these components automatically when changing theme

--- src/left/header/Header.jsx
+++ src/left/header/Header.jsx
@@ -8,23 +8,36 @@ export const Header = () => {
   const [hide2, setHide2] = useState("hide")
   const { ThemeSlice } = Stores()
   console.log(ThemeSlice);
+
+  const setDarkTheme = () => {
+    document.querySelector("body").setAttribute('data-theme', 'dark')
+  }
+
+  const setLightTheme = () => {
+    document.querySelector("body").setAttribute('data-theme', 'light')
+  }
+
   const handleHide = () => {
     if (hide1 === "") {
       setHide1("hide")
       setHide2("")
       ThemeSlice.changeThemeToLight()
+      setLightTheme()
     } else {
       setHide1("")
       setHide2("hide")
       ThemeSlice.changeThemeToDark()
+      setDarkTheme()
     }
   }
+
   return (
     <div className='Header'>
       <div className={`day_icon ${hide1}`} onClick={() => handleHide()}>
       </div>
       <div className={`night_icon ${hide2}`} onClick={() => handleHide()}>
       </div>
+      <div className="sun_bright_shadow"></div>
     </div>
   )
 }
