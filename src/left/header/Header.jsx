4aad183b8f1a264056fd5404fb92f11748707cf6
dani <danifromecuador@gmail.com>
2024-04-18 11:14:06 -0500

    Create ThemeSlice for managing Theme color changes

--- src/left/header/Header.jsx
+++ src/left/header/Header.jsx
@@ -1,17 +1,22 @@
 // src/left/header/Header.jsx
 import { useState } from 'react'
+import { Stores } from '../../store/store.js'
 import './Header.css'
 
 export const Header = () => {
   const [hide1, setHide1] = useState("")
   const [hide2, setHide2] = useState("hide")
+  const { ThemeSlice } = Stores()
+  console.log(ThemeSlice);
   const handleHide = () => {
     if (hide1 === "") {
       setHide1("hide")
       setHide2("")
+      ThemeSlice.changeThemeToLight()
     } else {
       setHide1("")
       setHide2("hide")
+      ThemeSlice.changeThemeToDark()
     }
   }
   return (
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,55 @@
 // src/store/store.js
 import { create } from "zustand";
+import { devtools } from 'zustand/middleware';
 
-export const store = create(()=>({
-  bears:23,
-}))
+// const createBearSlice = (set) => ({
+//   bears: 24,
+//   increaseBears: () => set((state) => ({
+//     BearSlice: { ...state.BearSlice, bears: state.BearSlice.bears + 1 },
+//     CowSlice: { cows: state.CowSlice.cows - 1 }
+//   }))
+// });
+
+// const createCowSlice = () => ({
+//   cows: 7,
+// });
+
+// export const Stores = create(devtools((set) => ({
+//   BearSlice: createBearSlice(set),
+//   CowSlice: createCowSlice(),
+// })));
+
+const lightTheme = {
+  backgroundLeft: "#a6f6af",
+  backgroundRight: "#fffbb7",
+  color1: "black",
+  color2: "gray",
+  color3: "green"
+}
+
+const darkTheme = {
+  backgroundLeft: "black",
+  backgroundRight: "gray",
+  color1: "white",
+  color2: "gray",
+  color3: "green"
+}
+
+const ThemeSlice = (set) => ({
+  theme: darkTheme,
+  changeThemeToLight: () => set((state) => ({
+    ThemeSlice: { ...state.ThemeSlice, theme: lightTheme }
+  })),
+  changeThemeToDark: () => set((state) => ({
+    ThemeSlice: { ...state.ThemeSlice, theme: darkTheme }
+  })),
+})
+
+const BearSlice = () => ({
+  bears: 23,
+})
+
+export const Stores = create(devtools((set) => ({
+  ThemeSlice: ThemeSlice(set),
+  BearSlice: BearSlice()
+})))
