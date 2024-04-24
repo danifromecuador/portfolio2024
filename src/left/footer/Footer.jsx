7785f6ac048be2ff038d0718eba2ea53cdb32932
dani <danifromecuador@gmail.com>
2024-04-24 12:40:06 -0500

    Add 4 social media icons on Footer

--- src/left/footer/Footer.jsx
+++ src/left/footer/Footer.jsx
@@ -1,10 +1,18 @@
-// src/
+// src/letf/footer/Footer.jsx
+import { Stores } from '../../store/store'
 import './Footer.css'
 
 export const Footer = () => {
+  const { theme } = Stores().ThemeSlice
+  console.log(theme);
   return (
     <div className='Footer'>
-      Footer Component
+      <div className="icons">
+        <div className={`${theme} linkedin`}></div>
+        <div className={`${theme} github`}></div>
+        <div className={`${theme} gmail`}></div>
+        <div className={`${theme} whatsapp`}></div>
+      </div>
     </div>
   )
 }
