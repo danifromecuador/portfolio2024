a56298b4536c9151a6a3bc4d1758d614f7bb4411
dani <danifromecuador@gmail.com>
2024-04-30 23:32:46 -0500

    Add links to social media icons

--- src/left/footer/Footer.jsx
+++ src/left/footer/Footer.jsx
@@ -8,10 +8,10 @@ export const Footer = () => {
   return (
     <div className='Footer'>
       <div className="icons">
-        <div className={`${theme} linkedin`}></div>
-        <div className={`${theme} github`}></div>
-        <div className={`${theme} gmail`}></div>
-        <div className={`${theme} whatsapp`}></div>
+        <a className={`${theme} linkedin`} href="https://www.linkedin.com/in/danifromec/" target='_blank'></a>
+        <a className={`${theme} github`} href="https://github.com/danifromecuador" target='_blank'></a>
+        <a className={`${theme} gmail`} href="mailto:danifromecuador@gmail.com"></a>
+        <a className={`${theme} whatsapp`} href="https://wa.me/593980022515" target='_blank'></a>
       </div>
     </div>
   )
