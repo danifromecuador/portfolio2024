bc2dc4c91313a38b57cbcab426df221f99a43920
dani <danifromecuador@gmail.com>
2024-09-20 21:20:49 -0500

    Move main files from main folder to src folder

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+// src/main/App.jsx
+import { Header } from './left/header/Header.jsx'
+import { MainComponent } from './left/mainComponent/MainComponent.jsx'
+import { Navbar } from './left/navbar/Navbar.jsx'
+import { Footer } from './left/footer/Footer.jsx'
+import { About } from './right/about/About.jsx'
+import { Projects } from './right/projects/Projects.jsx'
+import { Skills } from './right/skills/Skills.jsx'
+import { Contact } from './right/contact/Contact.jsx'
+import './App.css'
+
+export const App = () => {
+  return (
+    <div className="app">
+      <div className="left">
+        <Header />
+        <MainComponent />
+        <Navbar />
+        <Footer />
+      </div>
+      <div className="right">
+        <div><About /></div>
+        <div><Projects /></div>
+        <div><Skills /></div>
+        <div><Contact /></div>
+      </div>
+    </div>
+  )
+}
