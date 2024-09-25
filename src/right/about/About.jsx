ae504158e014d0a9297c1e58cf0bcd8135da774f
dani <danifromecuador@gmail.com>
2024-09-25 18:32:44 -0500

    Create the body of about me section

--- src/right/about/About.jsx
+++ src/right/about/About.jsx
@@ -3,7 +3,19 @@ import './About.css'
 export const About = () => {
   return (
     <div className='About' id='about'>
-      About Component
+      <h1>I'm a Full Stack Web Developer!</h1>
+      <p>With a background in Mechatronics and Electronics Engineeering. I the last year, I've spent 1200+ hours coding and working in a remote environment, collaborating with a distributed team around the world, using pair programming, async communication and daily stand ups</p>
+      <div className="projects">
+        <div className="todo">
+          TODO LIST
+        </div>
+        <div className="calculator">
+          CALCULATOR
+        </div>
+        <div className="tic-tac-toe">
+          TIC TAC TOE
+        </div>
+      </div>
     </div>
   )
 }
