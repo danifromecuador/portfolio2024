6e22df06dbdaba19efa85ac545e9cc4e2eee42a1
dani <danifromecuador@gmail.com>
2024-09-26 11:26:53 -0500

    Manage TodoList stuff using Zustand store

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,18 @@
 import { create } from "zustand";
 import { devtools } from 'zustand/middleware';
+import { TodoList } from "../right/about/miniprojects/TodoList";
 
 const ThemeSlice = (set) => ({
   theme: JSON.parse(localStorage.getItem("theme")) || "light",
 
   changeThemeToLight: () => {
     localStorage.setItem("theme", JSON.stringify("light"))
-    set((state) => ({ThemeSlice: { ...state.ThemeSlice, theme: "light" }}))
+    set((state) => ({ ThemeSlice: { ...state.ThemeSlice, theme: "light" } }))
   },
 
   changeThemeToDark: () => {
     localStorage.setItem("theme", JSON.stringify("dark"))
-    set((state) =>({ThemeSlice: { ...state.ThemeSlice, theme: "dark" }}))
+    set((state) => ({ ThemeSlice: { ...state.ThemeSlice, theme: "dark" } }))
   },
 })
 
@@ -19,7 +20,20 @@ const BearSlice = () => ({
   bears: 23,
 })
 
+const TodoListSlice = (set) => ({
+  todos: [],
+  add: (input) => (set(state => ({
+    TodoList: {
+      ...state.TodoList,
+      todos: [...state.TodoList.todos, {
+        id: Date.now(), text: input, completed: false
+      }]
+    }
+  })))
+})
+
 export const Stores = create(devtools((set) => ({
   ThemeSlice: ThemeSlice(set),
-  BearSlice: BearSlice()
+  BearSlice: BearSlice(),
+  TodoList: TodoListSlice(set)
 })))
