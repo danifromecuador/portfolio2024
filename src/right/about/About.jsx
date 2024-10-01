a5fae58a3909eeb2fe3dccf72949c1cf48b4728a
dani <danifromecuador@gmail.com>
2024-10-01 10:09:50 -0500

    Fix ESLint errors

--- src/right/about/About.jsx
+++ src/right/about/About.jsx
@@ -3,8 +3,8 @@ import './About.css'
 export const About = () => {
   return (
     <div className='About' id='about'>
-      <h1>I'm a Full Stack Web Developer!</h1>
-      <p>With a background in Mechatronics and Electronics Engineeering. I the last year, I've spent 1200+ hours coding and working in a remote environment, collaborating with a distributed team around the world, using pair programming, async communication and daily stand ups</p>
+      <h1>I&apos;m a Full Stack Web Developer!</h1>
+      <p>With a background in Mechatronics and Electronics Engineeering. I the last year, I&apos;ve spent 1200+ hours coding and working in a remote environment, collaborating with a distributed team around the world, using pair programming, async communication and daily stand ups</p>
       <div className="projects">
         <div className="todo">
           TODO LIST
