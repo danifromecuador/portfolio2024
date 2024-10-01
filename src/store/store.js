d9dd27d8dd11a276423825c71851f86b198e0aa4
dani <danifromecuador@gmail.com>
2024-09-30 21:43:07 -0500

    Create a separate file for logic of the store file

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
-import { devtools } from 'zustand/middleware';
+import { devtools } from 'zustand/middleware'
+import { add, markAsDone, deleteAllCompleted } from "./todoListLogic.js"
 
 const ThemeSlice = (set) => ({
   theme: JSON.parse(localStorage.getItem("theme")) || "light",
@@ -17,40 +18,9 @@ const ThemeSlice = (set) => ({
 
 const TodoListSlice = set => ({
   todos: JSON.parse(localStorage.getItem("todoList")) || [],
-  add: (input) => (set(state => ({
-    TodoList: {
-      ...state.TodoList,
-      todos: [...state.TodoList.todos, {
-        id: Date.now(), text: input, completed: false
-      }]
-    }
-  }))),
-  markAsDone: (e) => (set(state => {
-    let index = 0
-    for (let i = 0; i < state.TodoList.todos.length; i++) {
-      if (state.TodoList.todos[i].id === e.id) index = i
-    }
-    let newTodos = [...state.TodoList.todos]
-    newTodos[index].completed === true ? newTodos[index].completed = false : newTodos[index].completed = true
-    return ({
-      TodoList: {
-        ...state.TodoList,
-        todos: newTodos
-      }
-    })
-  })),
-  deleteAllCompleted: () => (set(state => {
-    let notCompleted = []
-    for (let i = 0; i < state.TodoList.todos.length; i++) {
-      if (state.TodoList.todos[i].completed === false) notCompleted.push(state.TodoList.todos[i])
-    }
-    return ({
-      TodoList: {
-        ...state.TodoList,
-        todos: notCompleted
-      }
-    })
-  }))
+  add: (input) => add(set, input),
+  markAsDone: (e) => markAsDone(set, e),
+  deleteAllCompleted: () => deleteAllCompleted(set)
 })
 
 export const Stores = create(devtools((set) => ({
