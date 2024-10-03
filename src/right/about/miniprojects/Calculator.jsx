c631f3a09be3b555538160878938493aa5b0e3b5
dani <danifromecuador@gmail.com>
2024-10-03 11:44:12 -0500

    Create buttons and display for calculator

--- src/right/about/miniprojects/Calculator.jsx
+++ src/right/about/miniprojects/Calculator.jsx
@@ -1,9 +1,16 @@
 import './Calculator.css'
 
 export const Calculator = () => {
+  const keyPad = [7, 8, 9, '+', 4, 5, 6, '-', 1,2,3,'x', 0,'.', '=', '/']
   return (
     <div className='Calculator'>
-      Calculator Component
+      <h1>CAS1O</h1>
+      <div className='display'>0123456789</div>
+      <div className='keypad'>
+        {
+          keyPad.map(k => <button className='key button'>{k}</button>)
+        }
+      </div>
     </div>
   )
 }
