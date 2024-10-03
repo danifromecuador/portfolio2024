a7b5e31216356c07a7ad785f2160053c0ca75785
dani <danifromecuador@gmail.com>
2024-10-03 12:14:36 -0500

    Show in the display of the calculator the pressed keys

--- src/right/about/miniprojects/Calculator.jsx
+++ src/right/about/miniprojects/Calculator.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react'
 import './Calculator.css'
 
 export const Calculator = () => {
-  const keyPad = [7, 8, 9, '+', 4, 5, 6, '-', 1,2,3,'x', 0,'.', '=', '/']
+  const keyPad = '789+456-123x0.=/'.split('')
+  const [display, setDisplay] = useState('0')
+  const handleKey = k => {
+    // if (display === '0' && k === '0') setDisplay('123')
+    if (display === '0' && k !== '0') setDisplay(k)
+    if (display !== '0') setDisplay(display + k)
+    // setDisplay(display.toString() + k.toString())
+    // if (display[0] === 0 && display.length > 0) setDisplay(display.unshift())
+    console.log(k + 1)
+    console.log(display)
+  }
+
+  const handleOnOff = () => { display ? setDisplay("") : setDisplay("0") }
+
   return (
     <div className='Calculator'>
       <h1>CAS1O</h1>
-      <div className='display'>0123456789</div>
+      <button className='on-off button' onClick={() => handleOnOff()}>ON/OFF</button>
+      <div className='display'>{display}</div>
       <div className='keypad'>
         {
-          keyPad.map(k => <button className='key button'>{k}</button>)
+          keyPad.map(k => <button className='key button' onClick={() => handleKey(k)}>{k}</button>)
         }
       </div>
     </div>
