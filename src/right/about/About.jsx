5156c301af90dfca708aac8019bbdbd4e67a430a
dani <danifromecuador@gmail.com>
2024-10-22 23:26:54 -0500

    Avoid wrapping components in divs

--- src/right/about/About.jsx
+++ src/right/about/About.jsx
@@ -9,15 +9,9 @@ export const About = () => {
       <h1>I&apos;m a Full Stack Web Developer!</h1>
       <p>With a background in Mechatronics and Electronics Engineeering. I the last year, I&apos;ve spent 1200+ hours coding and working in a remote environment, collaborating with a distributed team around the world, using pair programming, async communication and daily stand ups</p>
       <div className="projects">
-        <div className="todo">
           <TodoList />
-        </div>
-        <div className="calculator">
           <Calculator />
-        </div>
-        <div className="tic-tac-toe">
           <TicTacToe />
-        </div>
       </div>
     </div>
   )
