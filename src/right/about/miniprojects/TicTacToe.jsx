2573cedb92d001b9b0a0698ca29651e46c619cbd
dani <danifromecuador@gmail.com>
2024-10-23 23:09:58 -0500

    Create an array to store grid values

--- src/right/about/miniprojects/TicTacToe.jsx
+++ src/right/about/miniprojects/TicTacToe.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react'
 import './TicTacToe.css'
 
 export const TicTacToe = () => {
+  const [array, setArray] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+  const handleClick = (i) => {
+    if (array[i] === 0) setArray(array, array[i] = "X")
+  }
+
   return (
     <div className='TicTacToe'>
-      TicTacToe Component
+      <h1>TIC TAC TOE</h1>
+      <div className="grid">
+        <button onClick={() => handleClick(0)}>{`${array[0] === 0 ? "" : array[0]}`}</button>
+        <button onClick={() => handleClick(1)}>{`${array[1] === 0 ? "" : array[1]}`}</button>
+        <button onClick={() => handleClick(2)}>{`${array[2] === 0 ? "" : array[2]}`}</button>
+        <button onClick={() => handleClick(3)}>{`${array[3] === 0 ? "" : array[3]}`}</button>
+        <button onClick={() => handleClick(4)}>{`${array[4] === 0 ? "" : array[4]}`}</button>
+        <button onClick={() => handleClick(5)}>{`${array[5] === 0 ? "" : array[5]}`}</button>
+        <button onClick={() => handleClick(6)}>{`${array[6] === 0 ? "" : array[6]}`}</button>
+        <button onClick={() => handleClick(7)}>{`${array[7] === 0 ? "" : array[7]}`}</button>
+        <button onClick={() => handleClick(8)}>{array[8]}</button>
+      </div>
     </div>
   )
 }
