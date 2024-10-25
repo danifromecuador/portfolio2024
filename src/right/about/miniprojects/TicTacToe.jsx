2aa29e432e8a45708914f6fd12968bc8f42df630
dani <danifromecuador@gmail.com>
2024-10-24 23:23:58 -0500

    Handle event on mouse down and on mouse up separately

--- src/right/about/miniprojects/TicTacToe.jsx
+++ src/right/about/miniprojects/TicTacToe.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './TicTacToe.css'
 
 export const TicTacToe = () => {
-  const [array, setArray] = useState([0, 0, 0, "O", 0, 0, 0, 0, 0])
+  const [array, setArray] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0])
 
   const handleClick = (i) => {
     if (array[i] === 0) {
       let newArray = [...array]
-      newArray[i] = "X"
+      newArray[i] = 'X'
       setArray(newArray)
     }
   }
@@ -16,15 +16,15 @@ export const TicTacToe = () => {
     <div className='TicTacToe'>
       <h1>TIC TAC TOE</h1>
       <div className="grid">
-        <button onClick={() => handleClick(0)}>{`${array[0] === 0 ? "" : array[0]}`}</button>
-        <button onClick={() => handleClick(1)}>{`${array[1] === 0 ? "" : array[1]}`}</button>
-        <button onClick={() => handleClick(2)}>{`${array[2] === 0 ? "" : array[2]}`}</button>
-        <button onClick={() => handleClick(3)}>{`${array[3] === 0 ? "" : array[3]}`}</button>
-        <button onClick={() => handleClick(4)}>{`${array[4] === 0 ? "" : array[4]}`}</button>
-        <button onClick={() => handleClick(5)}>{`${array[5] === 0 ? "" : array[5]}`}</button>
-        <button onClick={() => handleClick(6)}>{`${array[6] === 0 ? "" : array[6]}`}</button>
-        <button onClick={() => handleClick(7)}>{`${array[7] === 0 ? "" : array[7]}`}</button>
-        <button onClick={() => handleClick(8)}>{`${array[8] === 0 ? "" : array[8]}`}</button>
+        <button onMouseDown={() => handleClick(0)}>{`${array[0] === 0 ? "" : array[0]}`}</button>
+        <button onMouseDown={() => handleClick(1)}>{`${array[1] === 0 ? "" : array[1]}`}</button>
+        <button onMouseDown={() => handleClick(2)}>{`${array[2] === 0 ? "" : array[2]}`}</button>
+        <button onMouseDown={() => handleClick(3)}>{`${array[3] === 0 ? "" : array[3]}`}</button>
+        <button onMouseDown={() => handleClick(4)}>{`${array[4] === 0 ? "" : array[4]}`}</button>
+        <button onMouseDown={() => handleClick(5)}>{`${array[5] === 0 ? "" : array[5]}`}</button>
+        <button onMouseDown={() => handleClick(6)}>{`${array[6] === 0 ? "" : array[6]}`}</button>
+        <button onMouseDown={() => handleClick(7)}>{`${array[7] === 0 ? "" : array[7]}`}</button>
+        <button onMouseDown={() => handleClick(8)}>{`${array[8] === 0 ? "" : array[8]}`}</button>
       </div>
     </div>
   )
